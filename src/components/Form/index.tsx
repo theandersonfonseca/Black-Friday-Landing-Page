a2165ce38dc1adc4c941d49b98e24a60f4da89d7
Anderson <andersonfonseca1999@hotmail.com>
2021-07-17 15:41:21 -0300

    Add error span

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,22 +6,29 @@ import { validateEmail } from '../../utils/validate'
 
 export default function Form() {
   const [value, setValue] = useState('')
+  const [error, setError] = useState(false)
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
 
-    if (!validateEmail(value)) return
+    if (!validateEmail(value)) {
+      setError(true)
+
+     return 
+    }
 
     window.localStorage.setItem('lojaTudoBaratoEmail', JSON.stringify(value))
     alert('Obrigado!')
 
     setValue('')
+    setError(false)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Receba nossas promoções antecipadamente!</h2>
       <input type="email" placeholder="Email" value={value} onChange={(e) => setValue(e.target.value)}/>
+      {error && <span className="error">Digite um email válido.</span>}
       <button type="submit">Confirmar</button>
     </form>
   )
